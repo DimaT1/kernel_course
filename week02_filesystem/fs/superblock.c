8c8d881300f3ac75adc952f5b08c67676348d9aa
DimaT1 <dymych99@gmail.com>
2020-02-09 19:54:06 +0300

    Remove superblock magic constants

--- week02_filesystem/fs/superblock.c
+++ week02_filesystem/fs/superblock.c
@@ -9,9 +9,10 @@ short read_superblock()
 	if (fseek(DISK_FILE, 0, SEEK_SET) != 0)
 		return 1;
 
-	uint64_t superblock[6];
+	uint64_t superblock[SUPERBLOCK_PARAMETERS];
 
-	if (fread(superblock, 8, 6, DISK_FILE) != 6)
+	if (fread(superblock, SUPERBLOCK_ATOM, SUPERBLOCK_PARAMETERS,
+		  DISK_FILE) != SUPERBLOCK_PARAMETERS)
 		return 1;
 
 	BLOCK_COUNT = superblock[0];
@@ -29,7 +30,7 @@ short write_superblock()
 	if (fseek(DISK_FILE, 0, SEEK_SET) != 0)
 		return 1;
 
-	uint64_t superblock[6];
+	uint64_t superblock[SUPERBLOCK_PARAMETERS];
 
 	superblock[0] = BLOCK_COUNT;
 	superblock[1] = INODE_COUNT;
@@ -38,7 +39,8 @@ short write_superblock()
 	superblock[4] = BLOCK_SIZE;
 	superblock[5] = INODE_SIZE;
 
-	if (fwrite(superblock, 8, 6, DISK_FILE) != 6)
+	if (fwrite(superblock, SUPERBLOCK_ATOM, SUPERBLOCK_PARAMETERS,
+		   DISK_FILE) != SUPERBLOCK_PARAMETERS)
 		return 1;
 
 	return 0;
